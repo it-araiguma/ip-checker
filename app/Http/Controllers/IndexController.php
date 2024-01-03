c5752915b70183dcbfd5ab6318c5fc2f7060ed59
mikepon <notnow618@gmail.com>
2024-01-03 21:04:01 +0900

    first commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        $ip = $request->ip();
+        $host = $request->host();
+        $userAgent = $request->userAgent();
+
+        return Inertia::render('hello-world', ['ip' => $ip, 'host' => $host, 'userAgent' => $userAgent]);
+    }
+}
---
+++ resources/js/Pages/hello-world.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+    ip: string,
+    host: string,
+    userAgent: string,
+}>();
+</script>
+
+<template>
+    <p class="greeting">{{ props.ip }}</p>
+    <p class="greeting">{{ props.host }}</p>
+    <p class="greeting">{{ props.userAgent }}</p>
+</template>
+
+<style>
+.greeting {
+    color: red;
+    font-weight: bold;
+}
+</style>
