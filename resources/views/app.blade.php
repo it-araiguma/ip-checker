755edaca4b98ab4e777985ad809f9cff29c60465
mikepon <notnow618@gmail.com>
2024-01-11 23:20:17 +0900

    アナリティクスタグ設置

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,20 @@
 <html>
 
 <head>
+    @if (config('app.env') === 'production')
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XFR2JYLS4T"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'G-XFR2JYLS4T');
+        </script>
+    @endif
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @inertiaHead
