80ebde6f8b86cf9195ef8b18630ad3f04e1786f5
it-araiguma <notnow618@gmail.com>
2024-07-18 22:47:25 +0900

    TWA関連のバージョン管理

---
+++ app/src/main/java/com/it_araiguma/ip/DelegationService.java
@@ -0,0 +1,14 @@
+package com.it_araiguma.ip;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
