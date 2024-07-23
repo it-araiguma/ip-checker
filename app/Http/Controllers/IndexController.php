3acd45f19a73863fe69a42be5426d84000de2360
it-araiguma <notnow618@gmail.com>
2024-07-23 21:49:45 +0900

    国の表示

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,16 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use GeoIp2\Database\Reader;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
-        $ip = $request->ip();
+        $ips = $request->getClientIps();
+        $ipv4 = null;
+        $ipv6 = null;
+
+        foreach ($ips as $ip) {
+            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+                $ipv4 = $ip;
+            } elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+                $ipv6 = $ip;
+            }
+        }
+
         $host = gethostbyaddr($ip);
         $userAgent = $request->userAgent();
 
-        return Inertia::render('Index', ['ip' => $ip, 'host' => $host, 'user_agent' => $userAgent]);
+        $country = null;
+        if ($ipv4) {
+            $reader = new Reader(storage_path('app/geoip/GeoLite2-Country.mmdb'));
+            try {
+                $record = $reader->country($ipv4);
+                $country = $record->country->name;
+            } catch (\Exception $e) {
+                $country = 'Unknown';
+            }
+        }
+
+        return Inertia::render('Index', [
+            'ipv4' => $ipv4,
+            'ipv6' => $ipv6,
+            'host' => $host,
+            'user_agent' => $userAgent,
+            'country' => $country,
+        ]);
     }
 
     public function privacy()
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { router } from "@inertiajs/vue3";
-import Header from "../Components/Header.vue";
-import Footer from "../Components/Footer.vue";
+import Header from "@/Components/Header.vue";
+import Footer from "@/Components/Footer.vue";
 
 const props = defineProps<{
-    ip: string,
+    ipv4: string,
+    ipv6: string,
     host: string,
     user_agent: string,
+    country: string,
 }>();
 
 const dense = ref(false);
@@ -23,8 +25,16 @@ const reload = () => {
         <q-page-container class="flex justify-center items-start">
             <div class="q-pa-md" style="width: 800px;">
                 <div class="q-gutter-y-md column">
+                    <div class="q-gutter-md" v-if="props.ipv4">
+                        <q-input label="IPアドレス(v4)" stack-label :dense="dense" readonly v-model="props.ipv4" />
+                    </div>
+
+                    <div class="q-gutter-md" v-if="props.ipv6">
+                        <q-input label="IPアドレス(v6)" stack-label :dense="dense" readonly v-model="props.ipv6" />
+                    </div>
+
                     <div class="q-gutter-md">
-                        <q-input label="IPアドレス" stack-label :dense="dense" readonly v-model="props.ip" />
+                        <q-input stack-label :dense="dense" label="国" readonly v-model="props.country" />
                     </div>
 
                     <div class="q-gutter-md">
